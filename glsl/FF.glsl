8a82b6ffcb70b188a753839a7308f213179e79c5
efim <efimmanevich@gmail.com>
2026-01-25 16:12:59 +0300

    fwidth

--- glsl/FF.glsl
+++ glsl/FF.glsl
@@ -1,9 +1,13 @@
 #define PI  3.14159265359
 #define TAU 6.28318530718
 
+//@FabriceNeyret2  comment
+#define S(y) smoothstep(fwidth(y), 0., abs(fract(y)-0.5) )
+
 
 float FF(vec2 p)
 {
+    /*
 	vec2 a = vec2(0.0, 0.5);  
     vec2 b = vec2(0.0, -0.5);
     float l1 = length(p-a);
@@ -14,24 +18,25 @@ float FF(vec2 p)
         res = 1.0/l1 - 1.0/l2;
     }
     res =  abs(res);
-    if (res > 1.)
+    if (res >= 1.)
         res = log(res)/3.;
     return res;    
+    */
+    //@FabriceNeyret2  comment
+    vec2 a = vec2(0, .5);  
+    float l1 = length(p-a),
+          l2 = length(p+a),
+         eps = .001, res = 1./eps;
+    if (l1 > eps && l2 > eps)
+        res = abs( 1./l1 - 1./l2 ); 
+    res = log((1. +  res));  //replace log(res)/3. to log((1. +  res));
+    return res;    
 }
-
-float dFF (vec2 p)
-{
-    float eps = 0.001;
-    float y = (FF(p) - FF(vec2(p.x, p.y + eps)))/eps;
-    float x = (FF(p) - FF(vec2(p.x+eps, p.y)))/eps;
-    float res = sqrt(x*x + y*y);
-    return res;
-}
-
 
 
 float EE(vec2 p)
 {
+    /*
     float eps = 0.001;
 	vec2 a = vec2(0.0, 0.5);  
     vec2 b = vec2(0.0, -0.5);
@@ -42,7 +47,25 @@ float EE(vec2 p)
     float cos2 = dot(z, l2);
     float res = (cos1 - cos2);    
     return res;
+    */
+    //@FabriceNeyret2  comment
+    vec2 a = vec2(0, .5);  
+    vec2 l1 = normalize(p-a),
+         l2 = normalize(p+a);
+    return l1.y - l2.y;
 }
+
+
+/*
+float dFF (vec2 p)
+{
+    float eps = 0.001;
+    float y = (FF(p) - FF(vec2(p.x, p.y + eps)))/eps;
+    float x = (FF(p) - FF(vec2(p.x+eps, p.y)))/eps;
+    float res = sqrt(x*x + y*y);
+    return res;
+}
+
 
 float dEE (vec2 p)
 {
@@ -52,16 +75,18 @@ float dEE (vec2 p)
     float res = sqrt(x*x + y*y);
     return res;
 }
+*/
 
 vec3 lines(vec2 p)
 {
-	
+	/*
     vec3 col0 = vec3(1., 1., 1.),colline = vec3(1., 0., 0.),colline2 = vec3(0., 0., 1.);
     float df = dFF(p);
     float de = dEE(p);
 
 	float y = fract(FF(p)*8.); 
-    float h = 0.03*df, eps = 15./iResolution.y*df; 
+    float h = 0.03*df; 
+    float eps = 15./iResolution.y*df; 
     float s1 = smoothstep(1. - h - eps, 1.-h, y),	
 	s2 = smoothstep(h, h-eps, y);
 	vec3 col1 = mix(col0, colline, s1);
@@ -75,6 +100,16 @@ vec3 lines(vec2 p)
 	vec3 col2 = mix(col0, colline2, s1);
 	col2 = mix(col2, colline2, s2);
 	return mix(col1, col2, 0.5);	
+    */
+    //@FabriceNeyret2  comment
+    float y = FF(p)*8. + iTime;	
+	vec3 col1 = mix(vec3(1), vec3(1,0,0), S(y) );
+
+    y = EE(p)*10. - iTime; 
+	vec3 col2 = mix(vec3(1), vec3(0,0,1), S(y) );
+	
+	return mix(col1, col2, .5);	
+    
 }
 
 
@@ -88,3 +123,4 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
 	vec3 col =  lines(p);
 	fragColor = vec4(col, 1.0);
 }
+
